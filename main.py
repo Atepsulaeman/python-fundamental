8dbcfe02bfe64b8684f348a7c9a9eb2f03230b85
atepsulaeman <kaysan2010@gmail.com>
2022-07-22 18:20:56 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Semua Sintaksis dasar bahasa Pemograman  diantaranya :
+1. Sekuensial : Langkah Berurutan
+2. Percabangan : Langkah Melompat Jika Kondisi Terpenuhi
+3. Perulangan : Mengulang Langkah Yang Sama Berkali -kali Selama/sampai Kondisi Terpenuhi
+"""
+# Sekuensial Tugas 1
+
+print('Ibu Memerinta,"Pergi Ke Tooko"')
+print('Anak Menjawab,"ok, pa yang harus saya Lakukan di Toko"')
+print ('Ibu Menjawab,"Tolong belikan satu botol susu,ditambah jika ada telur,belikan juga 6 butir telur,tapi jika susu tidak ada maka tidak jadi belanja"')
+print('Maka Anak berangkat ke toko mengecek barang dan harganya cukup')
+print('Anak Mulai Berbelanja')
+print('Anak pulang kerumah, "dan Menyerahkan hasil belanja"')
+
+# Percabangan
+jumlah_botol_susu = 173
+julah_telur = 1587
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+
