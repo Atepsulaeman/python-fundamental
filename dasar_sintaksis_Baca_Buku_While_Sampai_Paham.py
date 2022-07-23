34a479e2a5f7bfbb60bb3227ae867059ba3b28d6
atepsulaeman <kaysan2010@gmail.com>
2022-07-23 17:55:16 +0700

    sintaksis dasar while paham

--- dasar_sintaksis_Baca_Buku_While_Sampai_Paham.py
+++ dasar_sintaksis_Baca_Buku_While_Sampai_Paham.py
@@ -1,18 +1,20 @@
 """
 Program Perulangan membaaca buku dengan while sampai paham
 """
-jumlah_buku = 50
+jumlah_buku = 20
 print(f'jumlah Buku {jumlah_buku}')
 print('Ibu berkata, "baca semua buku')
 
 
-jumlah_buku_yang_sudah_dibaca = 0
-print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
 
-while jumlah_buku_yang_sudah_dibaca < jumlah_buku :
-    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
-    print(f"Buku Ke {jumlah_buku_yang_sudah_dibaca} sudah di Baca")
+while jumlah_buku_yang_sudah_dibaca_dan_dipahami < jumlah_buku :
+    jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
+    print(f"Buku Ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah di Baca dan dipahami")
 
-print ("SINTAKSIS MENGGUNAKAN WHILE atau SELAMA")
+## while : selama kondisi belum terpenuhi maka program berjalan sampai kondisi terpenuhi #
+
+print ("SINTAKSIS MENGGUNAKAN WHILE, adalah : Selama kondisi belum terpenuhi maka program akan berjalan sampai kondisi terpenuhi.")
 
 
