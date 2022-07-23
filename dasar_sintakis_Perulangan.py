04fbef70bd3fe3b9ebcb866b0e319e9fadba67e3
atepsulaeman <kaysan2010@gmail.com>
2022-07-23 12:41:39 +0700

    ini adalah sintaksis joke programmer

--- dasar_sintakis_Perulangan.py
+++ dasar_sintakis_Perulangan.py
@@ -4,13 +4,13 @@ Progrsam Perulangan membaaca buku
 jumlah_buku = 10
 print('Ibu berkata, "baca semua buku')
 
-jumlah_buku_yang_sydah_dibaca = 0
-print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sydah_dibca}')
+jumlah_buku_yang_sudah_dibaca = 0
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
 
-for jumlah_buku_yang_sydah_dibca in range(1,jumlah_buku+1):
-    print(f"Buku Ke {jumlah_buku_yang_sydah_dibca}Sudah dibaca")
+for jumlah_buku_yang_sudah_dibaca in range(1,jumlah_buku+1):
+    print(f"Buku Ke {jumlah_buku_yang_sudah_dibaca} Sudah dibaca")
 
-print(f'jumlah_buku_yang_sydah_dibaca {jumlah_buku_yang_sydah_dibaca} ')
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca} ')
 
 
 
