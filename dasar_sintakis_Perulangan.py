a19ede932187e2cdf3be187fe607e6d6af3d0c59
atepsulaeman <kaysan2010@gmail.com>
2022-07-23 12:10:39 +0700

    ini adalah sekuensial dan percabangan

---
+++ dasar_sintakis_Perulangan.py
@@ -0,0 +1,13 @@
+"""
+Progrsam Perulangan membaaca buku
+"""
+jumlah_buku = 10
+print('Ibu berkata, "baca semua buku')
+
+jumlah_buku_yang_sydah_dibaca = 0
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sydah_dibca}')
+
+for jumlah_buku_yang_sydah_dibca in range(1,jumlah_buku+1):
+    print(f"Buku Ke {jumlah_buku_yang_sydah_dibca}Sudah dibaca")
+
+print(f'jumlah_buku_yang_sydah_dibaca {jumlah_buku_yang_sydah_dibaca} ')
