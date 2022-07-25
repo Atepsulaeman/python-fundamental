478ee694655ca604d7f6bbe24d1fdc65dc79495c
atepsulaeman <kaysan2010@gmail.com>
2022-07-25 22:07:55 +0700

    sintaksis dasar while kondisi sudah dibaca

--- dasar_sintaksis_Membaca_buku_dengan_While.py
+++ dasar_sintaksis_Membaca_buku_dengan_While.py
@@ -6,6 +6,7 @@ print(f'jumlah Buku {jumlah_buku}')
 print('Ibu berkata, "baca semua buku')
 
 
+
 jumlah_buku_yang_sudah_dibaca = 0
 print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
 
@@ -13,6 +14,6 @@ while jumlah_buku_yang_sudah_dibaca < jumlah_buku :
     jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
     print(f"Buku Ke {jumlah_buku_yang_sudah_dibaca} sudah di Baca")
 
-print ("SINTAKSIS MENGGUNAKAN WHILE atau SELAMA")
+## note ("SINTAKSIS MENGGUNAKAN WHILE atau SELAMA")
 
 
