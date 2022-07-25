98700dbccd159b038d239c4ac3ad23534b7a452b
atepsulaeman <kaysan2010@gmail.com>
2022-07-25 16:33:29 +0700

    python while, if, else conditions in english

---
+++ sintaksis_type_data_list.py
@@ -0,0 +1,17 @@
+# DAFTAR_BUKU
+
+daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
+print('\nTampilkan Variable daftar_buku' )
+print(daftar_buku)
+
+
+for buku in (daftar_buku):
+    print(buku)
+
+print('\nTampilkan isi daftar_buku pada indeks tertentu')
+print(daftar_buku[0])
+print(daftar_buku[2])
+
+print('\nTampilkan dengan for i range')
+for i in range (0, len (daftar_buku)):
+    print(daftar_buku)
