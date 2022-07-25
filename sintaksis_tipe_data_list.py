30a5b3a579d306028e250f2f08049140b9e96422
atepsulaeman <kaysan2010@gmail.com>
2022-07-25 22:54:22 +0700

    tipe data list

--- sintaksis_tipe_data_list.py
+++ sintaksis_tipe_data_list.py
@@ -81,3 +81,9 @@ for i in range(0, len(daftar_buku)):
     print(daftar_buku[i]) # penjelasan kodenya : Daftar Buku yang akan dihapus 1 sampai 3, menyisakan buku terakhir 2 buku terakhir
 
 
+print('\n#.Perintah del dengan list comprehension start : END : STEP') # Pertanyaan : Pengen ada keterangan BUKU SUDAH DIHAPUS gimana?
+daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
+del daftar_buku[0: :2] # start : End : STEP # note : ( Menggunakan (: : ) Menghapus meloncati 1 step
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
