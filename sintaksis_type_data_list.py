db9214a3d3e4daa60eb19c7acefff69fb30e2dae
atepsulaeman <kaysan2010@gmail.com>
2022-07-25 16:55:09 +0700

    python while, if, else conditions in english

--- sintaksis_type_data_list.py
+++ sintaksis_type_data_list.py
@@ -13,5 +13,17 @@ print(daftar_buku[0])
 print(daftar_buku[2])
 
 print('\nTampilkan dengan for i range')
-for i in range (0, len (daftar_buku)):
-    print(daftar_buku)
+for i in range(0,len (daftar_buku)):
+    print(daftar_buku[i])
+
+daftar_buku =[ 1, 'Kenzi 2',313,-312]
+print('\nTampilkan dengan for i range, dimana tipe data tiap elemen berbeda2')
+for i in range(0,len (daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nKembalikan Nilai awal daftar_buku')
+daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
+print('\nTambahkan 1 Buku Baru')
+daftar_buku.append ('Dasar Pemograman python')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
