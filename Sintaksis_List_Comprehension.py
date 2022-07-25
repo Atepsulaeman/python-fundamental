82694245e15fd8a42885ad2932aa200ec680d910
atepsulaeman <kaysan2010@gmail.com>
2022-07-25 23:40:37 +0700

    Tipe Data List

--- Sintaksis_List_Comprehension.py
+++ Sintaksis_List_Comprehension.py
@@ -1,22 +1,26 @@
 print('\n#Perintah del')
+print('=' *25)
 daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah'] # tambahkan nomer urut ke bawah gimana?
 del daftar_buku[1]
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\n#.Perintah del dengan list comprehension') # Pertanyaan : Pengen ada keterangan BUKU SUDAH DIHAPUS gimana?
+print('=' *25)
 daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
-del daftar_buku[:]
+del daftar_buku[:] #haslinya akan menghapus Semua
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\n#.Perintah del dengan list comprehension start : END') # Pertanyaan : Pengen ada keterangan BUKU SUDAH DIHAPUS gimana?
+print('=' *25)
 daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
 del daftar_buku[0:-2] # start : End # note : index dihitung dari nol tetapi jumlah tetap dihitung dari satu,# bisa juga diganti -1 dst = menyisakan paling ujung
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i]) # penjelasan kodenya : Daftar Buku yang akan dihapus 1 sampai 3, menyisakan buku terakhir 2 buku terakhir
 
 print('\n#.Perintah del dengan list comprehension start : END : STEP') # Pertanyaan : Pengen ada keterangan BUKU SUDAH DIHAPUS gimana?
+print('=' *25)
 daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
 del daftar_buku[0: :2] # start : End : STEP # note : ( Menggunakan (: : ) Menghapus meloncati 1 step
 for i in range(0, len(daftar_buku)):
@@ -24,12 +28,15 @@ for i in range(0, len(daftar_buku)):
 
 
 print('\n#.Membuat List Baru')
+print('=' *25)
 daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
-daftar_buku_baru = daftar_buku
+daftar_buku_baru = daftar_buku[:] # perintah = menghasilkan 2 variabel artinya 2 nama ditempat yang sama, solusinya harus pakai list comprehension []
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
 print('\n#.Membuat List Baru')
+print('=' *25)
 del daftar_buku[:]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
 
