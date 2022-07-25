bf5a4255defe4a412dfe9c67bdf48b9a87301114
atepsulaeman <kaysan2010@gmail.com>
2022-07-25 17:30:01 +0700

    python while, if, else conditions in english

--- sintaksis_type_data_list.py
+++ sintaksis_type_data_list.py
@@ -1,29 +1,58 @@
 # DAFTAR_BUKU
 
 daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
-print('\nTampilkan Variable daftar_buku' )
+print('\n#.Tampilkan Variable daftar_buku' )
 print(daftar_buku)
 
 
 for buku in (daftar_buku):
     print(buku)
 
-print('\nTampilkan isi daftar_buku pada indeks tertentu')
+print('\n#.Tampilkan isi daftar_buku pada indeks tertentu')
 print(daftar_buku[0])
 print(daftar_buku[2])
 
-print('\nTampilkan dengan for i range')
+print('\n#.Tampilkan dengan for i range')
 for i in range(0,len (daftar_buku)):
     print(daftar_buku[i])
 
 daftar_buku =[ 1, 'Kenzi 2',313,-312]
-print('\nTampilkan dengan for i range, dimana tipe data tiap elemen berbeda2')
+print('\n#.Tampilkan dengan for i range, dimana tipe data tiap elemen berbeda2')
 for i in range(0,len (daftar_buku)):
     print(daftar_buku[i])
 
-print('\nKembalikan Nilai awal daftar_buku')
+print('\n#.Kembalikan Nilai awal daftar_buku')
 daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
-print('\nTambahkan 1 Buku Baru')
+for i in range(0,len (daftar_buku)):
+    print(daftar_buku[i])
+
+print('\n#.Tambahkan 1 Buku Baru')
 daftar_buku.append ('Dasar Pemograman python')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\n#.Clear List')   ## perintah clear untuk menghapus list
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\n#.Ganti Elemen Pertama')
+daftar_buku =['Quantum ikhlas','Jalan langit','Secret','Jalan Allah']
+daftar_buku[0] = 'belajar Tazwid'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
+print('\n#.Ambil Elemen Ke-2')
+buku = daftar_buku.pop(1)  # perintah pop mengambil elemen di bagian list ke ?
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\n#.Buku yang diambil Tadi')
+print(buku)
+daftar_buku.pop()
+
+
+
+
+
