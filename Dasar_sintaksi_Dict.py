c9277882328355cc74f0f89a5db3f7441f66be06
atepsulaeman <kaysan2010@gmail.com>
2022-07-26 13:50:50 +0700

    Tipe Data Dictionery {}

---
+++ Dasar_sintaksi_Dict.py
@@ -0,0 +1,11 @@
+users = {  # {} artinya dictionery
+"id": 1,
+  'name':'Leanne Graham',
+  'username':'Bret',
+  'email':'Sincere@april.biz'
+}
+print(users)
+print(users['id'])  #[] adalah elemen dari dictionery, fungsinya untuk mengambil data berdasarkan text kuncinta
+print(users['name'])
+print(users['username'])
+print(users['email'])
