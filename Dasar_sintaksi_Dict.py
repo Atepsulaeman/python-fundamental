d4a10757c758c8ad5a86b2ef10c92a27e7a7c397
atepsulaeman <kaysan2010@gmail.com>
2022-07-26 14:40:42 +0700

    Tipe Data Dictionery {}

--- Dasar_sintaksi_Dict.py
+++ Dasar_sintaksi_Dict.py
@@ -1,11 +1,33 @@
 users = {  # {} artinya dictionery
 "id": 1,
-  'name':'Leanne Graham',
-  'username':'Bret',
-  'email':'Sincere@april.biz'
+  "name":"Leanne Graham",
+  "username":"Bret",
+  "email":"Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+  }
 }
+
 print(users)
-print(users['id'])  #[] adalah elemen dari dictionery, fungsinya untuk mengambil data berdasarkan text kuncinta
-print(users['name'])
-print(users['username'])
-print(users['email'])
+print("="*20)
+print(users["id"])  #[] adalah elemen dari dictionery, fungsinya untuk mengambil data berdasarkan text kuncinta
+print("="*20)
+print(users["name"])
+print("="*20)
+print(users["username"])
+print("="*20)
+print(users["email"])
+print("="*20)
+print(users["address"]["street"])
+print("="*20)
+print(users["address"]["suite"])
+print("="*20)
+print(users["address"]["city"])
+print("="*20)
+print(users["address"]["zipcode"])
+
+
+
